3df30242ac54d65706c09f5f2ccec31fe317050d
prewittdavon <davonprewitt@lawn-128-61-56-238.lawn.gatech.edu>
2018-12-02 19:06:20 -0500

    Adds search show

--- visitor/SearchShow.java
+++ visitor/SearchShow.java
@@ -13,9 +13,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import common.AtlantaZoo;
+import java.sql.Connection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.sql.Date;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.beans.property.SimpleStringProperty;
+
 
 public class SearchShow extends VBox {
-
+  ObservableList<Show> data =
+      FXCollections.observableArrayList();
   public SearchShow() {
       Label title = new Label("Shows");
 
@@ -26,18 +37,54 @@ public class SearchShow extends VBox {
 
       DatePicker date = new DatePicker();
 
-      HBox exhibit = new HBox();
-      Label exhibitLabel = new Label("Exhbit");
-      ComboBox<String> exhibitDropDown = new ComboBox<String>();
-      exhibitDropDown.getItems().addAll("Yes", "No");
-      exhibit.getChildren().addAll(exhibitLabel, exhibitDropDown);
+      VBox exhibit = dropDown("Exhibit");
+      //exhibitDropDown.getItems().addAll("Yes", "No");
+      //exhibit.getChildren().addAll(exhibitLabel, exhibitDropDown);
 
       Button search = new Button();
       search.setText("Search");
       search.setOnAction(new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent event) {
-            //Implement
+          data.clear();
+          String nameField = nameText.getText();
+
+          VBox numAnBox1 = exhibit;
+          ComboBox dropEx = (ComboBox) numAnBox1.getChildren().get(1);
+          String ex = (String) dropEx.getValue();
+
+          String dt = date.getValue().toString();
+
+          System.out.println(ex);
+
+          String query = "select " +
+                         "Name, dt, Exhibit " +
+                         "from Shows WHERE " +
+                         "Name=\"" + nameField + "\"" +
+                         " AND Exhibit=\"" + ex + "\"";
+          Connection con = null;
+          try {
+              con = AtlantaZoo.conn();
+              Statement stmt = con.createStatement();
+              ResultSet rs = stmt.executeQuery(query);
+
+              while (rs.next()) {
+                  String name = rs.getString("Name");
+                  String exhibit = rs.getString("Exhibit");
+                  Date date = rs.getDate("dt");
+                  data.add(new Show(name, date.toString(), exhibit));
+                  //System.out.println(name + " " + exhibit );
+              }
+          } catch (Exception e) {
+              //JDBCTutorialUtilities.printSQLException(e);
+              System.out.println(e.getMessage());
+          } finally {
+            try {
+              if(con != null) con.close();
+            } catch (Exception f) {
+
+            }
+          }
         }
       });
 
@@ -52,48 +99,103 @@ public class SearchShow extends VBox {
       TableView table = new TableView();
       table.setEditable(true);
       TableColumn nameCol = new TableColumn("Name");
-      TableColumn exhibitsCol = new TableColumn("Exhibit");
+      nameCol.setCellValueFactory(
+              new PropertyValueFactory<>("name"));
+      TableColumn exhibitsCol = new TableColumn("Exhibits");
+      exhibitsCol.setCellValueFactory(
+              new PropertyValueFactory<>("exhibit"));
       TableColumn dateCol = new TableColumn("Date");
-      // TableColumn nameCol = new TableColumn("Name");
-      // TableColumn sizeCol = new TableColumn("Size");
-      // TableColumn numAnimalsCol = new TableColumn("Num Animals");
-      // TableColumn waterFeatureCol = new TableColumn("Water Feature");
+      dateCol.setCellValueFactory(
+              new PropertyValueFactory<>("date"));
 
-      // ObservableList<Person> data =
-      //   FXCollections.observableArrayList(
-      //       new Person("Jacob", "Smith", "jacob.smith@example.com"),
-      //       new Person("Isabella", "Johnson", "isabella.johnson@example.com"),
-      //       new Person("Ethan", "Williams", "ethan.williams@example.com"),
-      //       new Person("Emma", "Jones", "emma.jones@example.com"),
-      //       new Person("Michael", "Brown", "michael.brown@example.com")
-      //   );
-      //
-      // table.getColumns().addAll(nameCol, sizeCol, numAnimalsCol, waterFeatureCol);
-
-
+      getData();
+      table.setItems(data);
       table.getColumns().addAll(nameCol, exhibitsCol, dateCol);
 
-      Button logVisit = new Button();
-      logVisit.setText("Search");
-      logVisit.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent event) {
-            //Implement
-        }
-      });
+      // Button logVisit = new Button();
+      // logVisit.setText("Search");
+      // logVisit.setOnAction(new EventHandler<ActionEvent>() {
+      //   @Override
+      //   public void handle(ActionEvent event) {
+      //
+      //   }
+      // });
 
-      this.getChildren().addAll(title, row1, row2, table, logVisit);
+      this.getChildren().addAll(title, row1, row2, table);
 
   }
 
-  public VBox dropDown(String name, int min, int max) {
+  public VBox dropDown(String name) {
         Label label = new Label(name);
         ComboBox<String> dropDown = new ComboBox<String>();
-        for (int i = min; i <= max; i++) dropDown.getItems().add("" + i);
+        for (String s : AtlantaZoo.getExhibit()) dropDown.getItems().add(s);
+        //for (int i = 0; i <= max; i++) dropDown.getItems().add("" + i);
         dropDown.setValue(null);
         VBox box = new VBox();
         box.getChildren().addAll(label, dropDown);
         return box;
+  }
+
+  public static class Show {
+      private final SimpleStringProperty name;
+      private final SimpleStringProperty date;
+      private final SimpleStringProperty exhibit;
+
+      public Show(String name, String date, String exhibit) {
+          this.name = new SimpleStringProperty(name);
+          this.date = new SimpleStringProperty(date);
+          this.exhibit = new SimpleStringProperty(exhibit);
+      }
+
+      public String getName() {
+          return name.get();
+      }
+      public void setName(String uName) {
+          name.set(uName);
+      }
+
+      public String getDate() {
+          return date.get();
+      }
+      public void setDate(String d) {
+          date.set(d);
+      }
+
+      public String getExhibit() {
+          return exhibit.get();
+      }
+      public void setExhibit(String e) {
+          exhibit.set(e);
+      }
+  }
+
+  public void getData() {
+    String query = "select " +
+                   "Name, dt, Exhibit " +
+                   "from Shows";
+    Connection con = null;
+    try {
+        con = AtlantaZoo.conn();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+
+        while (rs.next()) {
+            String name = rs.getString("Name");
+            String exhibit = rs.getString("Exhibit");
+            Date date = rs.getDate("dt");
+            data.add(new Show(name, date.toString(), exhibit));
+            //System.out.println(name + " " + exhibit );
+        }
+    } catch (Exception e) {
+        //JDBCTutorialUtilities.printSQLException(e);
+        System.out.println(e.getMessage());
+    } finally {
+      try {
+        if(con != null) con.close();
+      } catch (Exception f) {
+
+      }
+    }
   }
 
 }
